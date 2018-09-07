0c9a0ef22bc509caada69881dc3f198e68109110
Jacob Persson <jacob.persson@gmail.com>
2018-09-07 16:45:20 +0200

    Make generated code public

--- Scripts/compile-protos.sh
+++ Scripts/compile-protos.sh
@@ -8,8 +8,8 @@ echo "Found carthage folder in ${CARTHAGE}"
 cd ${CARTHAGE}/Checkouts/generic-message-proto/ios/
 cp ./../proto/messages.proto ./
 mkdir -p ./tmp
-cat ios.proto messages.proto > ./tmp/messages.proto
-/usr/local/bin/protoc --proto_path=./tmp messages.proto --swift_out="${BASE_FOLDER}/Protos/"
+cp messages.proto ./tmp/messages.proto
+/usr/local/bin/protoc --swift_opt=FileNaming=DropPath --swift_opt=Visibility=Public --swift_out="${BASE_FOLDER}/Protos/" ./tmp/messages.proto
 rm -rf ./tmp
 rm messages.proto
 
@@ -17,8 +17,8 @@ rm messages.proto
 cd ${CARTHAGE}/Checkouts/backend-api-protobuf/ios/
 cp ./../proto/otr.proto ./
 mkdir -p ./tmp
-cat ios.proto otr.proto > ./tmp/otr.proto
-/usr/local/bin/protoc --proto_path=./tmp ./tmp/otr.proto --swift_out="${BASE_FOLDER}/Protos/"
+cp otr.proto ./tmp/otr.proto
+/usr/local/bin/protoc --swift_opt=FileNaming=DropPath --swift_opt=Visibility=Public ./tmp/otr.proto --swift_out="${BASE_FOLDER}/Protos/"
 rm -rf ./tmp
 rm otr.proto
 
