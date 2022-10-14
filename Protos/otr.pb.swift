75aa8d33931c47c0f23f607ccd4cfb63fe63f2a4
David Henner <david.henner@wire.com>
2022-10-14 11:44:50 +0200

    generate commit bundle protos

--- Protos/otr.pb.swift
+++ Protos/otr.pb.swift
@@ -1,6 +1,6 @@
 //
 // Wire
-// Copyright (C) 2021 Wire Swiss GmbH
+// Copyright (C) 2022 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -86,6 +86,80 @@ public enum Proteus_Priority: SwiftProtobuf.Enum {
 #if swift(>=4.2)
 
 extension Proteus_Priority: CaseIterable {
+  // Support synthesized by the compiler.
+}
+
+#endif  // swift(>=4.2)
+
+public enum Proteus_GroupInfoType: SwiftProtobuf.Enum {
+  public typealias RawValue = Int
+  case publicGroupState // = 1
+  case groupInfo // = 2
+  case groupInfoJwe // = 3
+
+  public init() {
+    self = .publicGroupState
+  }
+
+  public init?(rawValue: Int) {
+    switch rawValue {
+    case 1: self = .publicGroupState
+    case 2: self = .groupInfo
+    case 3: self = .groupInfoJwe
+    default: return nil
+    }
+  }
+
+  public var rawValue: Int {
+    switch self {
+    case .publicGroupState: return 1
+    case .groupInfo: return 2
+    case .groupInfoJwe: return 3
+    }
+  }
+
+}
+
+#if swift(>=4.2)
+
+extension Proteus_GroupInfoType: CaseIterable {
+  // Support synthesized by the compiler.
+}
+
+#endif  // swift(>=4.2)
+
+public enum Proteus_RatchetTreeType: SwiftProtobuf.Enum {
+  public typealias RawValue = Int
+  case full // = 1
+  case delta // = 2
+  case reference // = 3
+
+  public init() {
+    self = .full
+  }
+
+  public init?(rawValue: Int) {
+    switch rawValue {
+    case 1: self = .full
+    case 2: self = .delta
+    case 3: self = .reference
+    default: return nil
+    }
+  }
+
+  public var rawValue: Int {
+    switch self {
+    case .full: return 1
+    case .delta: return 2
+    case .reference: return 3
+    }
+  }
+
+}
+
+#if swift(>=4.2)
+
+extension Proteus_RatchetTreeType: CaseIterable {
   // Support synthesized by the compiler.
 }
 
@@ -556,6 +630,113 @@ public struct Proteus_OtrAssetMeta {
   fileprivate var _nativePush: Bool? = nil
 }
 
+public struct Proteus_GroupInfoBundle {
+  // SwiftProtobuf.Message conformance is added in an extension below. See the
+  // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+  // methods supported on all messages.
+
+  public var groupInfoType: Proteus_GroupInfoType {
+    get {return _groupInfoType ?? .publicGroupState}
+    set {_groupInfoType = newValue}
+  }
+  /// Returns true if `groupInfoType` has been explicitly set.
+  public var hasGroupInfoType: Bool {return self._groupInfoType != nil}
+  /// Clears the value of `groupInfoType`. Subsequent reads from it will return its default value.
+  public mutating func clearGroupInfoType() {self._groupInfoType = nil}
+
+  public var ratchetTreeType: Proteus_RatchetTreeType {
+    get {return _ratchetTreeType ?? .full}
+    set {_ratchetTreeType = newValue}
+  }
+  /// Returns true if `ratchetTreeType` has been explicitly set.
+  public var hasRatchetTreeType: Bool {return self._ratchetTreeType != nil}
+  /// Clears the value of `ratchetTreeType`. Subsequent reads from it will return its default value.
+  public mutating func clearRatchetTreeType() {self._ratchetTreeType = nil}
+
+  public var groupInfo: Data {
+    get {return _groupInfo ?? Data()}
+    set {_groupInfo = newValue}
+  }
+  /// Returns true if `groupInfo` has been explicitly set.
+  public var hasGroupInfo: Bool {return self._groupInfo != nil}
+  /// Clears the value of `groupInfo`. Subsequent reads from it will return its default value.
+  public mutating func clearGroupInfo() {self._groupInfo = nil}
+
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
+
+  public init() {}
+
+  fileprivate var _groupInfoType: Proteus_GroupInfoType? = nil
+  fileprivate var _ratchetTreeType: Proteus_RatchetTreeType? = nil
+  fileprivate var _groupInfo: Data? = nil
+}
+
+public struct Proteus_CommitBundle {
+  // SwiftProtobuf.Message conformance is added in an extension below. See the
+  // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+  // methods supported on all messages.
+
+  /// MlsMessage containing an MlsPlaintext Commit
+  public var commit: Data {
+    get {return _commit ?? Data()}
+    set {_commit = newValue}
+  }
+  /// Returns true if `commit` has been explicitly set.
+  public var hasCommit: Bool {return self._commit != nil}
+  /// Clears the value of `commit`. Subsequent reads from it will return its default value.
+  public mutating func clearCommit() {self._commit = nil}
+
+  /// MlsMessage containing a Welcome
+  public var welcome: Data {
+    get {return _welcome ?? Data()}
+    set {_welcome = newValue}
+  }
+  /// Returns true if `welcome` has been explicitly set.
+  public var hasWelcome: Bool {return self._welcome != nil}
+  /// Clears the value of `welcome`. Subsequent reads from it will return its default value.
+  public mutating func clearWelcome() {self._welcome = nil}
+
+  public var groupInfoBundle: Proteus_GroupInfoBundle {
+    get {return _groupInfoBundle ?? Proteus_GroupInfoBundle()}
+    set {_groupInfoBundle = newValue}
+  }
+  /// Returns true if `groupInfoBundle` has been explicitly set.
+  public var hasGroupInfoBundle: Bool {return self._groupInfoBundle != nil}
+  /// Clears the value of `groupInfoBundle`. Subsequent reads from it will return its default value.
+  public mutating func clearGroupInfoBundle() {self._groupInfoBundle = nil}
+
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
+
+  public init() {}
+
+  fileprivate var _commit: Data? = nil
+  fileprivate var _welcome: Data? = nil
+  fileprivate var _groupInfoBundle: Proteus_GroupInfoBundle? = nil
+}
+
+#if swift(>=5.5) && canImport(_Concurrency)
+extension Proteus_Priority: @unchecked Sendable {}
+extension Proteus_GroupInfoType: @unchecked Sendable {}
+extension Proteus_RatchetTreeType: @unchecked Sendable {}
+extension Proteus_UserId: @unchecked Sendable {}
+extension Proteus_QualifiedUserId: @unchecked Sendable {}
+extension Proteus_ClientId: @unchecked Sendable {}
+extension Proteus_ClientEntry: @unchecked Sendable {}
+extension Proteus_UserEntry: @unchecked Sendable {}
+extension Proteus_QualifiedUserEntry: @unchecked Sendable {}
+extension Proteus_NewOtrMessage: @unchecked Sendable {}
+extension Proteus_QualifiedNewOtrMessage: @unchecked Sendable {}
+extension Proteus_QualifiedNewOtrMessage.OneOf_ClientMismatchStrategy: @unchecked Sendable {}
+extension Proteus_ClientMismatchStrategy: @unchecked Sendable {}
+extension Proteus_ClientMismatchStrategy.ReportAll: @unchecked Sendable {}
+extension Proteus_ClientMismatchStrategy.IgnoreAll: @unchecked Sendable {}
+extension Proteus_ClientMismatchStrategy.ReportOnly: @unchecked Sendable {}
+extension Proteus_ClientMismatchStrategy.IgnoreOnly: @unchecked Sendable {}
+extension Proteus_OtrAssetMeta: @unchecked Sendable {}
+extension Proteus_GroupInfoBundle: @unchecked Sendable {}
+extension Proteus_CommitBundle: @unchecked Sendable {}
+#endif  // swift(>=5.5) && canImport(_Concurrency)
+
 // MARK: - Code below here is support for the SwiftProtobuf runtime.
 
 fileprivate let _protobuf_package = "proteus"
@@ -564,6 +745,22 @@ extension Proteus_Priority: SwiftProtobuf._ProtoNameProviding {
   public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
     1: .same(proto: "LOW_PRIORITY"),
     2: .same(proto: "HIGH_PRIORITY"),
+  ]
+}
+
+extension Proteus_GroupInfoType: SwiftProtobuf._ProtoNameProviding {
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .same(proto: "PUBLIC_GROUP_STATE"),
+    2: .same(proto: "GROUP_INFO"),
+    3: .same(proto: "GROUP_INFO_JWE"),
+  ]
+}
+
+extension Proteus_RatchetTreeType: SwiftProtobuf._ProtoNameProviding {
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .same(proto: "FULL"),
+    2: .same(proto: "DELTA"),
+    3: .same(proto: "REFERENCE"),
   ]
 }
 
@@ -1260,6 +1457,116 @@ extension Proteus_OtrAssetMeta: SwiftProtobuf.Message, SwiftProtobuf._MessageImp
     if lhs.recipients != rhs.recipients {return false}
     if lhs._isInline != rhs._isInline {return false}
     if lhs._nativePush != rhs._nativePush {return false}
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension Proteus_GroupInfoBundle: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = _protobuf_package + ".GroupInfoBundle"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .standard(proto: "group_info_type"),
+    2: .standard(proto: "ratchet_tree_type"),
+    3: .standard(proto: "group_info"),
+  ]
+
+  public var isInitialized: Bool {
+    if self._groupInfoType == nil {return false}
+    if self._ratchetTreeType == nil {return false}
+    if self._groupInfo == nil {return false}
+    return true
+  }
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
+      switch fieldNumber {
+      case 1: try { try decoder.decodeSingularEnumField(value: &self._groupInfoType) }()
+      case 2: try { try decoder.decodeSingularEnumField(value: &self._ratchetTreeType) }()
+      case 3: try { try decoder.decodeSingularBytesField(value: &self._groupInfo) }()
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    // The use of inline closures is to circumvent an issue where the compiler
+    // allocates stack space for every if/case branch local when no optimizations
+    // are enabled. https://github.com/apple/swift-protobuf/issues/1034 and
+    // https://github.com/apple/swift-protobuf/issues/1182
+    try { if let v = self._groupInfoType {
+      try visitor.visitSingularEnumField(value: v, fieldNumber: 1)
+    } }()
+    try { if let v = self._ratchetTreeType {
+      try visitor.visitSingularEnumField(value: v, fieldNumber: 2)
+    } }()
+    try { if let v = self._groupInfo {
+      try visitor.visitSingularBytesField(value: v, fieldNumber: 3)
+    } }()
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: Proteus_GroupInfoBundle, rhs: Proteus_GroupInfoBundle) -> Bool {
+    if lhs._groupInfoType != rhs._groupInfoType {return false}
+    if lhs._ratchetTreeType != rhs._ratchetTreeType {return false}
+    if lhs._groupInfo != rhs._groupInfo {return false}
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension Proteus_CommitBundle: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = _protobuf_package + ".CommitBundle"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .same(proto: "commit"),
+    2: .same(proto: "welcome"),
+    3: .standard(proto: "group_info_bundle"),
+  ]
+
+  public var isInitialized: Bool {
+    if self._commit == nil {return false}
+    if self._groupInfoBundle == nil {return false}
+    if let v = self._groupInfoBundle, !v.isInitialized {return false}
+    return true
+  }
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
+      switch fieldNumber {
+      case 1: try { try decoder.decodeSingularBytesField(value: &self._commit) }()
+      case 2: try { try decoder.decodeSingularBytesField(value: &self._welcome) }()
+      case 3: try { try decoder.decodeSingularMessageField(value: &self._groupInfoBundle) }()
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    // The use of inline closures is to circumvent an issue where the compiler
+    // allocates stack space for every if/case branch local when no optimizations
+    // are enabled. https://github.com/apple/swift-protobuf/issues/1034 and
+    // https://github.com/apple/swift-protobuf/issues/1182
+    try { if let v = self._commit {
+      try visitor.visitSingularBytesField(value: v, fieldNumber: 1)
+    } }()
+    try { if let v = self._welcome {
+      try visitor.visitSingularBytesField(value: v, fieldNumber: 2)
+    } }()
+    try { if let v = self._groupInfoBundle {
+      try visitor.visitSingularMessageField(value: v, fieldNumber: 3)
+    } }()
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: Proteus_CommitBundle, rhs: Proteus_CommitBundle) -> Bool {
+    if lhs._commit != rhs._commit {return false}
+    if lhs._welcome != rhs._welcome {return false}
+    if lhs._groupInfoBundle != rhs._groupInfoBundle {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }
